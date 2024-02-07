62302727b1c05e40477650f9cc1d3bd716d93b35
grahampasmurf <36420136+grahampasmurf@users.noreply.github.com>
2024-02-07 15:04:22 -0600

    Initial commit.

---
+++ src/real_estate_dapp_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { real_estate_dapp_backend } from "../../declarations/real_estate_dapp_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await real_estate_dapp_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
